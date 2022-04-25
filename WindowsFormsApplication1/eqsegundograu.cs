44becfb27994662f1d530e46bcb831221a18b639
mileny velho <82714273+milenyv@users.noreply.github.com>
2022-04-25 13:14:07 -0300

    Add files via upload

---
+++ WindowsFormsApplication1/eqsegundograu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class eqsegundograu
+    {
+        private String a;
+        private String b;
+        private String c;
+
+        public void seta(string _a) { a = _a; }
+        public String geta() { return a; }
+        public void setb(string _b) { b = _b; }
+        public String getb() { return b; }
+        public void setc(string _c) { c = _c; }
+        public String getc() { return c; }
+    }
+}
---
+++ WindowsFormsApplication1/equaçãoUML.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class equaçãoUML
+    {
+         public partial class Eqsegundograu : Form
+    {
+        equaçãoUML equação = new equaçãoUML();
+        public equação()
+        {
+            eqsegundograu();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            texta.Clear();
+            textb.Clear();
+            textc.Clear();
+            text.Clear();
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+            Eqsegundograu.seta(texta.Text);
+            Eqsegundograu.setb(textb.Text);
+            Eqsegundograu.setc(textc.Text);
+            Eqsegundograu.validaDados();
+
+            if (Erro.getErro())
+            {
+                MessageBox.Show(Erro.getMens());
+            }
+            else
+            {
+                MessageBox.Show("Seus dados foram salvos com sucesso.");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            texta.Text = Eqsegundograu.geta();
+            textb.Text = Eqsegundograu.getb();
+            textc.Text = Eqsegundograu.getc();
+           
+}
